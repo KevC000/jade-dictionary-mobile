c774daaa9be05dc51c7d675a6b48c739623bbb39
KevC000 <chenkevin1119@gmail.com>
2024-01-11 17:17:55 -0500

    initial

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,12 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
-};
+  extends: ['@react-native-community', 'plugin:prettier/recommended'],
+  rules: {
+    'no-console': 'warn', // Warn instead of error on console.log
+    'react/prop-types': 'off', // Turn off prop-types rule for React components
+    'no-unused-vars': 'warn', // Warn for unused variables instead of error
+    'react/react-in-jsx-scope': 'off', // Not needed with React 17+ JSX Transform
+    'prettier/prettier': ['warn', {}, { usePrettierrc: true }], // Use Prettier rules as warnings
+    // You can add more rules or adjust existing ones here
+  },
+}
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,7 +1,10 @@
 module.exports = {
-  arrowParens: 'avoid',
-  bracketSameLine: true,
-  bracketSpacing: false,
-  singleQuote: true,
-  trailingComma: 'all',
-};
+  semi: false, // Do not automatically insert semicolons
+  singleQuote: true, // Use single quotes instead of double quotes
+  printWidth: 120, // Increase maximum line length if needed
+  tabWidth: 2, // Set the number of spaces per indentation-level
+  useTabs: false, // Indent lines with spaces, not tabs
+  bracketSpacing: true, // Print spaces between brackets
+  jsxBracketSameLine: false, // Put > on the last line instead of at a new line
+  trailingComma: 'none', // Remove trailing commas
+}
